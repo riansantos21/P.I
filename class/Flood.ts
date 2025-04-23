fd09053de9b3f8918f8be7d231f78e6dd96ea283
João Pedro <jpappelt10@gmail.com>
2025-04-23 20:33:16 -0300

    Update Flood.ts

--- class/Flood.ts
+++ class/Flood.ts
@@ -1,20 +1,27 @@
 export class Flood {
+    private id_flood:number
     private name: string;
     private date: Date;
 
     constructor(
-        name: string, 
-        date: Date      
+        id_flood:number,
+        name:string,
+        date:Date      
 ) {
+        this.id_flood = id_flood;
         this.name = name;
         this.date = date;
     }
 
+    setId_Flood(id_flood:number):void{this.id_flood = id_flood
+    };
     setName(name: string): void {this.name = name; 
     };
     setDate(date: Date): void {this.date = date; 
     };
 
+    getId_Flood():number{return this.id_flood
+    };
     getName(): string {return this.name; 
     };
     getDate(): Date {return this.date; 
