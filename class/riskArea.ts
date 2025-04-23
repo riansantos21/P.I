8eddf64d8851b6a39cf5b251a50c2fc29b4ee659
João Pedro <jpappelt10@gmail.com>
2025-04-23 20:41:42 -0300

    Update riskArea.ts

--- class/riskArea.ts
+++ class/riskArea.ts
@@ -1,23 +1,34 @@
 export class riskArea{
-    private name:string;
-    private description:string;
+    private id_riskArea:number
+    private name:string
+    private description:string
 
-    constructor(nameConst:string,descriptionConst:string){
+    constructor(id_riskArea:number,nameConst:string,descriptionConst:string){
+        this.id_riskArea = id_riskArea
         this.name = nameConst
         this.description = descriptionConst
-    };
+    }
 
+    setID_riskArea(id_riskArea:number):void{
+        this.id_riskArea = id_riskArea
+    }
     setName(nameConst:string):void{ 
     this.name = nameConst
-    };
+    }
     setDescription(nameConst:string):void{
         this.name = nameConst
-    };
+    }
+
+    getId_riskArea():number{
+        return this.id_riskArea
+    }
     getName():string{
         return this.name
-    };
+    }
     getDescription():string{
         return this.description
-    };
+    }
 
 }
+
+
